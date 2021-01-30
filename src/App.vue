49f23414ce4ec6c74ef38d12ba35dd9d4986d9af
mayuxian <mayuxian@autohome.com.cn>
2021-01-30 21:04:11 +0800

    已经调通teleport了. teleport的to不能指向自身的父元素

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <div
+    id="teleport"
+    class="teleport"
+    style="height: 100px; background: gray"
+  ></div>
+  <div  style="position: relative">
+    <h3>Tooltips with Vue 3 Teleport</h3>
+    <div>
+      <modal-button></modal-button>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import { defineComponent } from 'vue'
+import ModalButton from './components/ModalButton.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+export default defineComponent({
+  components: {
+    ModalButton,
+  },
+})
 </script>
 
 <style>
